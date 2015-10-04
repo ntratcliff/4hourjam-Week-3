f70cbd802654ba93c7eb63ed1ae2fab93a3730a1
Noah Ratcliff <me@nratcliff.com>
2015-10-04 14:12:09 -0400

    Camera works

---
+++ 4hourjam Week 3/Assets/Resources/src/DelayedKill.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DelayedKill : MonoBehaviour
+{
+    public bool RandomDelay;
+    public float RandSecondsMin, RandSecondsMax;
+    public float SecondsDelay;
+    // Use this for initialization
+    void Start()
+    {
+        if (RandomDelay)
+            SecondsDelay = Random.Range(RandSecondsMin, RandSecondsMax);
+
+        StartCoroutine("Kill", SecondsDelay);
+    }
+
+    IEnumerator Kill(float delay)
+    {
+        yield return new WaitForSeconds(delay);
+        GameObject.Destroy(this.gameObject);
+    }
+}
