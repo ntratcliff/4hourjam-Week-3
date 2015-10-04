f95c18b6b94ffc964ba6b5db07c037e82e477a09
Noah Ratcliff <me@nratcliff.com>
2015-10-04 14:46:37 -0400

    Voxel 'splosions works

--- 4hourjam Week 3/Assets/Resources/src/Explode.cs
+++ 4hourjam Week 3/Assets/Resources/src/Explode.cs
@@ -3,15 +3,48 @@ using System.Collections;
 
 public class Explode : MonoBehaviour
 {
-
+    public int Voxels;
+    public Vector3 VeloMin;
+    public Vector3 VeloMax;
+    GameObject voxPrefab;
     // Use this for initialization
     void Start()
     {
+        voxPrefab = Resources.Load<GameObject>("prefab/Voxel");
+        if (!voxPrefab)
+        {
+            Debug.LogError("Failed to load Voxel prefab");
+        }
 
+        StartCoroutine(TestExplode());
     }
 
     public void DoExplode()
     {
+        Destroy(GetComponent<BoxCollider>());
+        Destroy(GetComponent<MeshRenderer>());
+        for (int i = 0; i < Voxels; i++)
+        {
+            StartCoroutine(MakeVoxel());
+        }
+        Destroy(gameObject);
+    }
 
+    IEnumerator MakeVoxel()
+    {
+        GameObject voxel = Instantiate(voxPrefab);
+        Vector3 range = VeloMax - VeloMin;
+        Vector3 scale = Random.insideUnitSphere;
+        range.Scale(scale);
+        if (range.y < 0)
+            range.y = -range.y; 
+        voxel.GetComponent<Rigidbody>().velocity = range;
+        yield return null;
+    }
+
+    IEnumerator TestExplode()
+    {
+        yield return new WaitForSeconds(3);
+        DoExplode();
     }
 }
